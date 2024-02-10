b39bdb25faf503d9c6aa5ebbc53ed9b4099ab775
Durga Suresh <durgavs2020@gmail.com>
2024-02-09 21:21:59 -0800

    reverse number

--- PracticeJava/src/main/java/PrimeNumberInARange.java
+++ PracticeJava/src/main/java/PrimeNumberInARange.java
@@ -1,7 +1,10 @@
+package main.java;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+
 public class PrimeNumberInARange {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
