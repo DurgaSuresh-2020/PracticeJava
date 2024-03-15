bcf5d35a8fb48fab4a26749380bd80818d05e6e1
Durga Suresh <durgavs2020@gmail.com>
2024-03-15 11:18:06 -0700

    array

--- PracticeJava/src/main/java/ContiguousSubArrayWithLargestSum.java
+++ PracticeJava/src/main/java/ContiguousSubArrayWithLargestSum.java
@@ -1,36 +1,27 @@
 package main.java;
 
-import java.util.*;
+public class ContiguousSubArrayWithLargestSum {
 
-public class FindLengthOfLongestConsecutiveNumbersInArray {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>(Arrays.asList(0, 3, 2, 5, 6, 4, 9, 11, 12, 13, 15,17,16,14 , 18));
-        list = list.stream().distinct().sorted().toList();
-        System.out.println(list);
 
-        List<Integer> currentConsecutiveNumbers = new ArrayList<>();
-        List<Integer> longestConsecutiveNumbers = new ArrayList<>();
+        int[] arr = {1, 2, 3, 6, 7, 8, 0};
+        int maxSum = arr[0];
+        int currentSum = arr[0];
 
-        for (int i = 1; i < list.size(); i++) {
-            currentConsecutiveNumbers.add(list.get(i-1));
-            if (list.get(i) == list.get(i - 1) + 1) {
-                currentConsecutiveNumbers.add(list.get(i));
+        for (int i = 1; i < arr.length; i++) {
+            // If adding the current element to the current subarray improves the sum, extend the subarray.
+            // Otherwise, start a new subarray with the current element.
+            if (arr[i] > currentSum + arr[i]) {
+                currentSum = arr[i];
             } else {
-                if (currentConsecutiveNumbers.size() > longestConsecutiveNumbers.size()) {
-                    longestConsecutiveNumbers = new ArrayList<>(currentConsecutiveNumbers);
-                }
-                currentConsecutiveNumbers.clear();
+                currentSum = currentSum + arr[i];
+            }
+
+            // Update the maximum sum encountered so far.
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
             }
         }
-
-       // Check if the last sequence is the longest
-        if (currentConsecutiveNumbers.size() > longestConsecutiveNumbers.size()) {
-            longestConsecutiveNumbers = new ArrayList<>(currentConsecutiveNumbers);
-        }
-
-        longestConsecutiveNumbers = longestConsecutiveNumbers.stream().distinct().toList();
-
-        System.out.println("Longest consecutive sequence: " + longestConsecutiveNumbers);
-        System.out.println("Length of the longest consecutive sequence: " + longestConsecutiveNumbers.size());
     }
 }
+
