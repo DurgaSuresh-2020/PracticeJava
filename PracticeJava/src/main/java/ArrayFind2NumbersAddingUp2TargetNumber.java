b371ab5fa1357a30d7d45d697f226643c8b6006f
Durga Suresh <durgavs2020@gmail.com>
2024-03-15 11:56:57 -0700

    Array

--- PracticeJava/src/main/java/ArrayFind2NumbersAddingUp2TargetNumber.java
+++ PracticeJava/src/main/java/ArrayFind2NumbersAddingUp2TargetNumber.java
@@ -1,2 +1,29 @@
-package main.java;public class ArrayFind2NumbersAddingUp2TargetNumber {
+package main.java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayFind2NumbersAddingUp2TargetNumber {
+
+    public static void main(String[] args){
+
+
+    List<Integer> arr = new ArrayList<>(Arrays.asList(1, 2, 3, 6, 7, 8, 0));
+    int targetSum = 8;
+    List<Integer> subArr = new ArrayList<>();
+
+    for(int i = 0;i<arr.size();i++){
+        if(arr.contains(targetSum -arr.get(i))){
+            subArr.add(arr.get(i));
+            subArr.add(arr.get(arr.indexOf(targetSum- arr.get(i))));
+            break;
+        }
+
+    }
+
+    System.out.println(subArr);
+
+    }
+
 }
